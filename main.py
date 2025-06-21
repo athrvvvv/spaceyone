d648162e1177981f3220917250ce734a55b8f3ae
athrv chaulkar <98272709+athrvvvv@users.noreply.github.com>
2025-06-21 23:16:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,12 @@ schedule.every().day.at("21:00").do(send_daily_price)
 def handle_info(message):
     if message.from_user.id == USER_ID:
         p = price()
-        bot.reply_to(message, f"Price Found to be: {p} ✅\n🛜 {url}")
+        bot.reply_to(message, (
+            f"🔔 *Price Update Found!*\n\n"
+            f"💰 *Incredible Price:* `{p}`\n"
+            f"🛍️ [Soundcore Space One - Incredible]({url})\n"
+            f"🎧 [JBL Tune Live - Amazon](https://amzn.eu/d/73fgzvN)\n"
+        ), parse_mode="Markdown")
 
 def run_schedule():
     while True:
